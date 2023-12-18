01ea0d11309bbcebfeab4db3674eab99b9759ff9
santos-eric <esantosdev77@gmail.com>
2023-12-18 05:08:47 -0700

    changed app.js to arrow function, created hello world message and tested on android

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  const message = 'Hello World';
+
+	return (
+		<View style={{height: 100, width:100, backgroundColor: 'orange'}}>
+      <Text>{ message}</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
+export default App;
