5d60d9b302827ce84b3e66cc98910fb96a5ff455
santos-eric <esantosdev77@gmail.com>
2023-12-18 05:43:02 -0700

    added app.js styles and hardcoded weather with some styles

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native'
 
 const App = () => {
-  const message = 'Hello World';
-
-	return (
-		<View style={{height: 100, width:100, backgroundColor: 'orange'}}>
-      <Text>{ message}</Text>
-		</View>
-	);
+  return (
+    // SafeAreaView is used to avoid the notch in the phone
+    <SafeAreaView style={styles.wrapper}>
+        <View style={styles.container}>
+          <Text style={styles.temp}>38F</Text>
+          <Text styles={styles.feels}>Feels Like 30F </Text>
+          <View style={styles.hiLowWrapper}>
+            <Text styles={styles.hiLow}>High: 64F</Text>
+            <Text styles={styles.hiLow}>Low: 38F</Text>
+        </View>
+        <View style={styles.bodyWrapper}>
+          <Text>It's Cloudy</Text>
+          <Text>Wear your jacket</Text>
+        </View>
+        </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+  wrapper: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  temperature: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black'
+  },
+  hiLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  hiLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 40
+  },
+  description: {
+    fontSize: 43
+  },
+  message: {
+    fontSize: 25
+  }
+})
 
-export default App;
+export default App
